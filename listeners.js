ad998e4ab1a8eb054ccb509a90855ce88dee5152
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-04-03 15:31:42 -0400

    added text revealing animation and header

--- listeners.js
+++ listeners.js
@@ -22,12 +22,8 @@ $(".pokemon-link").on("click", function(e){
 
     $(`#pokemon${area}`).html(`<div class=${animationDirectionClass}><div class="yAxis"><img id="pokemonImg${area}" src="${trainer.myPokemon[thisPokemon].frontImg}" alt="pokemon${area}"></div></div>`)
 
-    $(`#pokemon-info-${area}`).html(`<div><span class="statHeading">hp</span>: ${trainer.myPokemon[thisPokemon].hp}</div><div><span class="statHeading">attack</span>: ${trainer.myPokemon[thisPokemon].attack}</div><div><span class="statHeading">defense</span>: ${trainer.myPokemon[thisPokemon].defense}</div><div><span class="statHeading">abilities</span>: <span id="abilities-${area}"></span></div>`)
+    $(`#pokemon-info-${area}`).html(`<div id="info"><div><span class="statHeading">hp</span>: ${trainer.myPokemon[thisPokemon].hp}</div><div><span class="statHeading">attack</span>: ${trainer.myPokemon[thisPokemon].attack}</div><div><span class="statHeading">defense</span>: ${trainer.myPokemon[thisPokemon].defense}</div><div><span class="statHeading">abilities</span>: <span id="abilities-${area}"></span></div></div>`)
 
     trainer.myPokemon[thisPokemon].abilities.forEach(function(ability) {$(`#abilities-${area}`).append(ability + "    &nbsp;&nbsp;&nbsp;&nbsp;")})
-
-    $(`#pokemon-info-${area}`).show( "explode", {pieces: 16}, 6000 );
-    //$(`#pokemon-info-${area}`).toggle( "explode", {pieces: 16}, 750);
-
 
 })
