44111e71715c7ad64efd61d5865f17f44c1f3aa7
David <davidlangva@gmail.com>
2018-04-03 12:40:23 -0400

    animations working

--- listeners.js
+++ listeners.js
@@ -5,18 +5,22 @@ $(".pokemon-link").on("click", function(e){
     let thisPokemon = $(this).attr('id')
     let area
     let trainer
+    let animationDirectionClass
+
+    console.log(thisPokemon)
     
 
     if(/olga/.test(parentClass)){
         area = "1"
         trainer = olga
+        animationDirectionClass = 'moveRight'
     } else{
         area = "2"
         trainer = david
-        console.log(trainer)
+        animationDirectionClass = 'moveLeft'
     }
 
-    $(`#pokemon${area}`).html(`<img src="${trainer.myPokemon[thisPokemon].frontImg}" alt="pokemon${area}">`)
+    $(`#pokemon${area}`).html(`<div class=${animationDirectionClass}><div class="yAxis"><img id="pokemonImg${area}" src="${trainer.myPokemon[thisPokemon].frontImg}" alt="pokemon${area}"></div></div>`)
      
 
 })
