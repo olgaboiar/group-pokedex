74ecbac886b7cb8b38dc676a7612ab247bb97455
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-04-03 11:52:59 -0400

    changed pokemon icon id for link tag

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function renderPage(trainer1, trainer2) {
 function renderIcons(trainer) {
 	for(i=0; i<trainer.pokemonArray.length; i++) {
 		let thisPokemon = trainer.pokemonArray[i]
-		let $pokemonLinkTag = $('<a id="pokemon-' + trainer.myPokemon[thisPokemon].name + '" class="btn-floating btn-large waves-effect waves-light pokemon-link"><img id="pokemon-icon-' + trainer.myPokemon[thisPokemon].name + '" src="' + trainer.myPokemon[thisPokemon].frontImg + '">"</a>');
+		let $pokemonLinkTag = $('<a id=' + trainer.myPokemon[thisPokemon].name + '" class="btn-floating btn-large waves-effect waves-light pokemon-link"><img id="pokemon-icon-' + trainer.myPokemon[thisPokemon].name + '" src="' + trainer.myPokemon[thisPokemon].frontImg + '">"</a>');
 		let $pokemonIconTag = $('<div class="col s12 m4 center-align pokemon-icon"/>').append($pokemonLinkTag);
 		$(".pokemon-icons-" + trainer.name).append($pokemonIconTag);
 	}
